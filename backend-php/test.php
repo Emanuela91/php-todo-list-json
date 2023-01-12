6a76bf9ba61f464a72698d60bb0126d2673f5d1a
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-12 18:48:02 +0100

    creato array todo php e traformato anche in file json

---
+++ backend-php/test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $todoList = [
+        [
+            "text" => "Fare la spesa",
+            "completed" => true
+        ],
+        [
+            "text" => "Prendere l'aereo",
+            "completed" => true
+        ],
+        [
+            "text" => "Preparare le valigie",
+            "completed" => false
+        ],
+        [
+            "text" => "Prenotare l'hotel",
+            "completed" => false
+        ]
+    ];
+
+    // prendo array php e lo trasformo in file json cosi da leggerlo in vue 
+    $jsonTodoList = json_encode($todoList);
+    file_put_contents('todo.json', $jsonTodoList);
+
+    // prendo i file json e li traduco in php 
+    $jsonTodoList = file_get_contents("todo.json", true);
+    $todoList = json_decode($jsonTodoList);
+
+    // stampo a schermo l'array
+    var_dump($todoList);
+    ?>
+
+
+</body>
+
+</html>
--- frontend-client-vue/src/App.vue
+++ frontend-client-vue/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
+
 import axios from 'axios';
+
 </script>
 
 <template>
